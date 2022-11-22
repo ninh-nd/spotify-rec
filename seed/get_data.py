600a01292bc9000fddbf4aebcf2c498c9ea65390
ninh-nd <nguyendangninh0@gmail.com>
2022-11-22 19:36:18 +0700

    Reorganize folder structure

--- seed/get_data.py
+++ seed/get_data.py
@@ -9,7 +9,6 @@ from csv import writer
 load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-NUMBER_OF_THREADS = 10
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
@@ -32,13 +31,14 @@ def seed(start_year, end_year):
     for i in range(start_year, end_year + 1):
         print("Currently getting 1000 songs from year " + str(i))
         get_data(i, results, 0, 500)
-        time.sleep(2) # Avoid rate limiting
         get_data(i, results, 500, 1000)
         print("Finished getting 1000 songs from year " + str(i))
     # Save the results into a csv file
     with open('song_id.csv', 'w') as f:
         csv_writer = writer(f)
-        csv_writer.writerow(results)
+        csv_writer.writerow(['id'])
+        for i in range(0, len(results)):
+            csv_writer.writerow([results[i]])
         f.close()
     print("Number of songs obtained: " + str(len(results)))
 
---
+++ seed/seed_database.py
@@ -0,0 +1,39 @@
+import os
+import json
+import spotipy
+import pandas as pd
+import csv
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+# Initialize the Spotify API
+load_dotenv()
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# Load data from csv file
+df = pd.read_csv("song_id.csv")
+# Create a list of song ids
+song_ids = df['id'].tolist()
+results = []
+
+print("Getting audio features from API...")
+for i in range(0, len(song_ids), 100):
+    # Get the audio features in a batch of 100
+    audio_features = sp.audio_features(song_ids[i:i+100])
+    results += audio_features
+
+print("Saving audio features to csv file...")
+# Save the results into a csv file
+with open('song_features.csv', 'w') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(['id', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo', 'valence'])
+    
+    for i in range(0, len(results)):
+        if (results[i] is not None): # Some songs may not have audio features
+            csv_writer.writerow([results[i]['id'], results[i]['acousticness'], results[i]['danceability'], results[i]['energy'], results[i]['instrumentalness'], results[i]['liveness'], results[i]['loudness'], results[i]['speechiness'], results[i]['tempo'], results[i]['valence']])
+    
+    f.close()
+
+print("Finish saving audio features to csv file.")
