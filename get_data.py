11bb7bb5c7511bed153123e61ddbc3d299705d80
ninh-nd <nguyendangninh0@gmail.com>
2022-11-22 17:32:54 +0700

    Cleanup and add logging for clarity

--- get_data.py
+++ get_data.py
@@ -1,19 +1,25 @@
 import os
 import time
-import json
 import sys
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from csv import writer
+# Initialize the Spotify API
 load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 NUMBER_OF_THREADS = 10
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# Get commandline input, including start year and end year
 start_year = int(sys.argv[1])
 end_year = int(sys.argv[2])
+
+# Global variable to store the results
+results = []
+
 def get_data(year, results, start, end):
     query_string = "year:" + str(year)
     for i in range(start, end, 50):
@@ -21,19 +27,19 @@ def get_data(year, results, start, end):
         for j in range(0, 50):
             second_result = first_result[j]['id']
             results.append(second_result)
-results = []
-for i in range(start_year, end_year):
-    get_data(i, results, 0, 500)
-    time.sleep(2) # Avoid rate limiting
-    get_data(i, results, 500, 1000)
-# Save the results into a csv file
-with open('song_id.csv', 'w') as f:
-    csv_writer = writer(f)
-    csv_writer.writerow(results)
-    f.close()
-print(len(results))
-# print(json.dumps(results, indent=4))
-# print(json.dumps(playlist_id, indent=4))
-# result = sp.search(q='year:2010', type='track', limit=2, offset=1)['tracks']['items']
-# print(json.dumps(result, indent=4))
-# print(get_data(2019, results, 1000, 1500))
+
+def seed(start_year, end_year):
+    for i in range(start_year, end_year + 1):
+        print("Currently getting 1000 songs from year " + str(i))
+        get_data(i, results, 0, 500)
+        time.sleep(2) # Avoid rate limiting
+        get_data(i, results, 500, 1000)
+        print("Finished getting 1000 songs from year " + str(i))
+    # Save the results into a csv file
+    with open('song_id.csv', 'w') as f:
+        csv_writer = writer(f)
+        csv_writer.writerow(results)
+        f.close()
+    print("Number of songs obtained: " + str(len(results)))
+
+seed(start_year, end_year)
