931c7b6cd1917cfb45ed924820103551226b3c0e
ninh-nd <nguyendangninh0@gmail.com>
2022-11-29 21:44:22 +0700

    Initial data representation work

--- seed/seed_database.py
+++ seed/seed_database.py
@@ -28,11 +28,11 @@ print("Saving audio features to csv file...")
 # Save the results into a csv file
 with open('song_features.csv', 'w') as f:
     csv_writer = csv.writer(f)
-    csv_writer.writerow(['id', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo', 'valence'])
+    csv_writer.writerow(['id', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'speechiness', 'tempo', 'time_signature', 'valence'])
     
     for i in range(0, len(results)):
         if (results[i] is not None): # Some songs may not have audio features
-            csv_writer.writerow([results[i]['id'], results[i]['acousticness'], results[i]['danceability'], results[i]['energy'], results[i]['instrumentalness'], results[i]['liveness'], results[i]['loudness'], results[i]['speechiness'], results[i]['tempo'], results[i]['valence']])
+            csv_writer.writerow([results[i]['id'], results[i]['acousticness'], results[i]['danceability'], results[i]['energy'], results[i]['instrumentalness'], results[i]['key'], results[i]['liveness'], results[i]['loudness'], results[i]['mode'], results[i]['speechiness'], results[i]['tempo'], results[i]['time_signature'], results[i]['valence']])
     
     f.close()
 
